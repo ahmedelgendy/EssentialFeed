f57acf154745aa6d071db2402742a087286a57c5
Ahmed Elgendy <ahmed@letslooksie.com>
2022-10-02 20:54:51 +0200

    Initial Commit

---
+++ EssentialFeed/EssentialFeed.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeed.h
+//  EssentialFeed
+//
+//  Created by Ahmed Elgendy on 2.10.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeed.
+FOUNDATION_EXPORT double EssentialFeedVersionNumber;
+
+//! Project version string for EssentialFeed.
+FOUNDATION_EXPORT const unsigned char EssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeed/PublicHeader.h>
+
+
