58167e649ac979b8c71801a1657df132efb9b874
Ahmed Elgendy <ahmed@letslooksie.com>
2022-10-02 20:59:36 +0200

    Create feed item

---
+++ EssentialFeed/FeedFeature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Ahmed Elgendy on 2.10.2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let descriptin: String?
+    let location: String?
+    let imageURL: URL
+}
