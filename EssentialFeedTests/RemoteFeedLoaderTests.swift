cea9e82bc6c44ef26e9236e3e419c4a77b3a7bfd
Ahmed Elgendy <ahmed@letslooksie.com>
2022-10-03 19:57:12 +0200

    add RemoteFeedLoaderTests

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,49 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Ahmed Elgendy on 3.10.2022.
+//
+
+import XCTest
+@testable import EssentialFeed
+
+class RemoteFeedLoaderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+    
+    func test_init_doesntRequestDataFromURL() {
+        let url = URL(string: "https://google.com")!
+        let client = makeSUT(url: url).client
+        XCTAssertNil(client.url)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "https://google.com")!
+        let (sut, client) = makeSUT(url: url)
+        sut.load()
+        XCTAssertEqual(client.url, url)
+    }
+    
+    private func makeSUT(url: URL) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        
+        var url: URL?
+        
+        func get(from url: URL) {
+            self.url = url
+        }
+    }
+
+}
