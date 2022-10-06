440d70c0a35076a648b6b3bb771907ece7a910c9
Ahmed Elgendy <ahmed@letslooksie.com>
2022-10-06 18:37:44 +0200

    Move parse logics to a new type RemoteFeedMapper

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -41,10 +41,10 @@ public class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, urlResponse):
-                if urlResponse.statusCode == 200,
-                   let decodedData = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(decodedData.items.map(\.modelItem)))
-                } else {
+                do {
+                    let items = try RemoteFeedMapper.map(data: data, response: urlResponse)
+                    completion(.success(items))
+                } catch {
                     completion(.failure(.invalidData))
                 }
             case .failure(_):
@@ -55,22 +55,31 @@ public class RemoteFeedLoader {
     
 }
 
-private struct Root: Decodable {
-    let items: [Item]
-}
-
-private struct Item: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
-    
-    var modelItem: FeedItem {
-        FeedItem(
-            id: id,
-            description: description,
-            location: location,
-            imageURL: image
-        )
+class RemoteFeedMapper {
+    class func map(data: Data, response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else { throw RemoteFeedLoader.Error.invalidData }
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map(\.modelItem)
     }
+    
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var modelItem: FeedItem {
+            FeedItem(
+                id: id,
+                description: description,
+                location: location,
+                imageURL: image
+            )
+        }
+    }
+
 }
