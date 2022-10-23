35eae1c4af758212424bdab6e1117b0c776d7b1f
Ahmed Elgendy <ahmed@letslooksie.com>
2022-10-23 13:39:32 +0200

    Make storeURL an explicit dependency so we can inject test specific urls

--- EssentialFeedTests/Feed Store/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Store/CoreDataFeedStoreTests.swift
@@ -106,7 +106,8 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     // MARK: - HELPERS
     private func makeSUT() -> FeedStore {
         let bundle = Bundle(for: CoreDataFeedStore.self)
-        let sut = try! CoreDataFeedStore(bundle: bundle)
+        let storeURL = URL(fileURLWithPath: "/dev/null")
+        let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: bundle)
         trackForMemoryLeak(instance: sut)
         return sut
     }
