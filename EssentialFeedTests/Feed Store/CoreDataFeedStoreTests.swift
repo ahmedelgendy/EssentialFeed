e9927d03b33003039fe067c50152ffbc3ccb21a2
Ahmed Elgendy <ahmed@letslooksie.com>
2022-10-23 14:27:25 +0200

    Core Data store Side Effects run Serially

--- EssentialFeedTests/Feed Store/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Store/CoreDataFeedStoreTests.swift
@@ -65,8 +65,8 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_storeSideEffects_runSerially() {
-//        let sut = makeSUT()
-//        assertThatSideEffectsRunSerially(on: sut)
+        let sut = makeSUT()
+        assertThatSideEffectsRunSerially(on: sut)
     }
     
     // MARK: - HELPERS
