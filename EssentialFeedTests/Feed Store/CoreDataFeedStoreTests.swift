997c32c2fb3fc569183af850f849c1f4f6366a3e
Ahmed Elgendy <ahmed@letslooksie.com>
2022-10-23 12:52:33 +0200

    Conform to `FeedStoreSpecs`

--- EssentialFeedTests/Feed Store/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Store/CoreDataFeedStoreTests.swift
@@ -9,6 +9,7 @@ import XCTest
 import EssentialFeed
 
 class CoreDataFeedStore: FeedStore {
+    
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping DeletionCompletion) {
         
     }
@@ -23,9 +24,100 @@ class CoreDataFeedStore: FeedStore {
     
 }
 
-class CoreDataFeedStoreTests: XCTestCase {
-
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+//        assertThatRetrieveDeliversEmptyOnEmptyCache(on: makeSUT())
+    }
     
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+//        let sut = makeSUT()
+//        assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+//        let sut = makeSUT()
+//        assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+//        let sut = makeSUT()
+//        assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
+    }
+    
+    func test_retrieve_deliversFailureOnRetrievalError() {
+//        let sut = makeSUT()
+//        try! "invalid data".write(to: testSpecificStoreURL(), atomically: false, encoding: .utf8)
+//        assertThatRetrieveDeliversFailureOnRetrievalError(on: sut)
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnFailure() {
+//        let sut = makeSUT()
+//        try! "invalid data".write(to: testSpecificStoreURL(), atomically: false, encoding: .utf8)
+//        assertThatRetrieveHasNoSideEffectsOnFailure(on: sut)
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+//        let sut = makeSUT()
+//        assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+//        let sut = makeSUT()
+//        assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+//        let sut = makeSUT()
+//        assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
+    }
+    
+    func test_insert_deliversErrorOnInsertionError() {
+//        let storeURL = URL(string: "invalid://store-url")
+//        let sut = makeSUT(storeURL: storeURL)
+//        assertThatInsertDeliversErrorOnInsertionError(on: sut)
+    }
+    
+    func test_insert_hasNoSideEffectsOnInsertionError() {
+//        let storeURL = URL(string: "invalid://store-url")
+//        let sut = makeSUT(storeURL: storeURL)
+//        assertThatInsertHasNoSideEffectsOnInsertionError(on: sut)
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+//        let sut = makeSUT()
+//        assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+//        let sut = makeSUT()
+//        assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+//        let sut = makeSUT()
+//        assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+//        let sut = makeSUT()
+//        assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
+    }
+    
+    
+    func test_delete_deliversErrorOnDeletionError() {
+//        let sut = makeSUT(storeURL: homeDirectoryURL())
+//        assertThatDeleteDeliversErrorOnDeletionError(on: sut)
+    }
+    
+    func test_delete_hasNoSideEffectsOnDeletionError() {
+//        let sut = makeSUT(storeURL: homeDirectoryURL())
+//       assertThatDeleteHasNoSideEffectsOnDeletionError(on: sut)
+    }
+    
+    func test_storeSideEffects_runSerially() {
+//        let sut = makeSUT()
+//        assertThatSideEffectsRunSerially(on: sut)
+    }
     
     // MARK: - HELPERS
     private func makeSUT() -> FeedStore {
