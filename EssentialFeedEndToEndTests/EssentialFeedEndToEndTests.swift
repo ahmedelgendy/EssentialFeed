ff47d694859f0688e5ed2fa419b4a7cec9e3abb7
Ahmed Elgendy <ahmed@letslooksie.com>
2022-11-08 14:40:38 +0200

    Extract `feedTestServerURL` to reusable variable

--- EssentialFeedEndToEndTests/EssentialFeedEndToEndTests.swift
+++ EssentialFeedEndToEndTests/EssentialFeedEndToEndTests.swift
@@ -44,7 +44,7 @@ class EssentialFeedEndToEndTests: XCTestCase {
     }
 
     private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed/73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")!
+        let testServerURL = feedTestServerURL.appendingPathComponent("/73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedImageDataLoader(client: client)
         trackForMemoryLeak(instance: client, file: file, line: line)
@@ -63,9 +63,8 @@ class EssentialFeedEndToEndTests: XCTestCase {
     }
     
     private func getResult(file: StaticString = #filePath, line: UInt = #line) -> RemoteFeedLoader.Result? {
-        let url = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(url: url, client: client)
+        let loader = RemoteFeedLoader(url: feedTestServerURL, client: client)
         trackForMemoryLeak(instance: client, file: file, line: line)
         trackForMemoryLeak(instance: loader, file: file, line: line)
         let exp = expectation(description: "Result loaded")
@@ -78,6 +77,10 @@ class EssentialFeedEndToEndTests: XCTestCase {
         return expectedResult
     }
     
+    private var feedTestServerURL: URL {
+        return URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+    }
+
     private func expectedItem(at index: Int) -> FeedImage {
         return FeedImage(
             id: id(at: index),
