d998e18c70524e88d0423c3130c46858ccbc51ac
Ahmed Elgendy <ahmed@letslooksie.com>
2022-11-08 13:54:23 +0200

    Move `RemoteFeedImageDataLoader` to production

--- EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -8,62 +8,6 @@
 import XCTest
 import EssentialFeed
 
-public protocol FeedImageDataLoaderTask {
-    func cancel()
-}
-
-class RemoteFeedImageDataLoader {
-    private let client: HTTPClient
-   
-    public enum Error: Swift.Error {
-        case invalidData
-    }
-
-    init(client: HTTPClient) {
-        self.client = client
-    }
-    
-    @discardableResult
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        let task = HTTPTaskWrapper(completion)
-        task.wrapped = client.get(from: url) { [weak self] result in
-                guard self != nil else { return }
-                switch result {
-                case .success(let (data, response)):
-                    if response.statusCode == 200, !data.isEmpty {
-                        task.complete(with: .success(data))
-                    } else {
-                        task.complete(with: .failure(Error.invalidData))
-                    }
-                case .failure(let error):
-                    task.complete(with: .failure(error))
-                }
-            }
-        return task
-    }
-    
-    private final class HTTPTaskWrapper: FeedImageDataLoaderTask {
-        private var completion: ((FeedImageDataLoader.Result) -> Void)?
-        var wrapped: HTTPClientTask?
-        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-            self.completion = completion
-        }
-        
-        func complete(with result: FeedImageDataLoader.Result) {
-            completion?(result)
-        }
-        
-        func cancel() {
-            preventFurtherCompletions()
-            wrapped?.cancel()
-        }
-        
-        private func preventFurtherCompletions() {
-            completion = nil
-        }
-    }
-}
-
 class RemoteFeedImageDataLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestImageLoading() {
