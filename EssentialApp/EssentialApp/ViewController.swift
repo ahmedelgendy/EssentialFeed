3014d7b5137746ecc62c67b99a5131e8aa98f90d
Ahmed Elgendy <ahmed@letslooksie.com>
2022-11-12 13:21:44 +0200

    Add Essential App project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Ahmed Elgendy on 12.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
